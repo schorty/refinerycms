8fca8af22c050a1191cf7483479d17931a4d12bf
Daniel Krüger <d.krueger@thisisdmg.com>
2014-06-06 14:14:02 +0200

    Adding commit from Anita Graham to add custom presenters

--- pages/app/presenters/refinery/pages/content_page_presenter.rb
+++ pages/app/presenters/refinery/pages/content_page_presenter.rb
@@ -8,7 +8,7 @@ module Refinery
       def initialize(page, page_title)
         super()
         add_default_title_section(page_title) if page_title.present? && Pages.show_title_in_body
-        add_page_parts(page.parts) if page
+        add_page_parts(page) if page
         add_default_post_page_sections
       end
 
@@ -23,10 +23,35 @@ module Refinery
         add_section_if_missing(:id => :side_body)
       end
 
-      def add_page_parts(parts)
-        parts.each do |part|
-          add_section PagePartSectionPresenter.new(part)
+      def add_page_parts(page)
+        page.parts.each do |part|
+
+          # Some section titles have spaces!
+          section_name = part.title.gsub(/\s+/,'')
+          # find an instance variable or attribute named for this page part
+          section_data = find_data(page, section_name)
+
+          # find a presenter for this section
+          section_presenter = find_presenter("Refinery::Pages::#{section_name.capitalize}SectionPresenter")
+
+          if section_presenter && section_data
+            # augment the data to make it more like a page part
+            section_data = {id: part.title, data:section_data } unless section_data.nil?
+          else
+            section_presenter = PagePartSectionPresenter
+            section_data = part
+          end
+
+          add_section section_presenter.new(section_data) unless section_data.nil?
         end
+      end
+
+      def find_presenter(presenter_name)
+        presenter_name.constantize if Object.const_defined?(presenter_name)
+      end
+
+      def find_data(page, part_name)
+        page.respond_to?(part_name) ? page.send(part_name) : instance_variable_get('@'+part_name)
       end
     end
   end
